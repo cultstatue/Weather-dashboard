a0f7ef1d0323de6c865f83be6c7cce83559f18bf
cultstatue <saltedchurros@gmail.com>
2022-06-28 16:00:27 -0400

    added geoloacting api request

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,4 @@
+// capturing hard-codes elements
 var mainContainer = document.querySelector("#main-container");
 var searchContainer = document.querySelector("#search-container");
 var weatherContainer = document.querySelector("weather-container");
@@ -20,3 +21,16 @@ var inputTextEl = document.createElement("p");
 inputTextEl.className = "control has-icons-left";
 inputTextEl.innerHTML = "<input class='input' type='text' placeholder='Search city name'> <span class ='icon is-left'><i class='fas fa-search' aria-hidden='true'></i></span>"
 formContainer.appendChild(inputTextEl);
+
+// function to get location data
+var getLocationData = function(cityName) {
+
+    var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q="+ cityName +"&limit=5&appid=f36d17786468fcf6dab864e03af92392"
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+          console.log(data);
+        });
+      });
+}
+
+getLocationData("london");
