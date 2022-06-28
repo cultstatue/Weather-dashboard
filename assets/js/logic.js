877dadab6d82df7369bda369a877a320c909db6d
cultstatue <saltedchurros@gmail.com>
2022-06-28 16:39:12 -0400

    added ability to get city location info through submit form

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -19,18 +19,42 @@ searchBox.appendChild(formContainer);
 
 var inputTextEl = document.createElement("p");
 inputTextEl.className = "control has-icons-left";
-inputTextEl.innerHTML = "<input class='input' type='text' placeholder='Search city name'> <span class ='icon is-left'><i class='fas fa-search' aria-hidden='true'></i></span>"
+inputTextEl.innerHTML = "<form id ='input-form'><input class='input' id='cityname' type='text' placeholder='Search city name'> <span class ='icon is-left'><i class='fas fa-search' aria-hidden='true'></i></span></form>"
 formContainer.appendChild(inputTextEl);
+
+var inputForm = document.querySelector("#cityname")
+
 
 // function to get location data
 var getLocationData = function(cityName) {
 
-    var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q="+ cityName +"&limit=5&appid=f36d17786468fcf6dab864e03af92392"
+    var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=1&appid=f36d17786468fcf6dab864e03af92392";
     fetch(apiUrl).then(function(response) {
         response.json().then(function(data) {
           console.log(data);
         });
+
       });
+
 }
 
-getLocationData("london");
+// add event listender to submission form
+searchContainer.addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+    var cityName = inputForm.value.trim().toLowerCase();
+    console.log(cityName);
+
+    if (cityName) {
+
+        getLocationData(cityName);
+        inputForm.value = " ";
+    } else {
+
+        alert("Please enter a valid city name")
+
+    }
+
+});
+
